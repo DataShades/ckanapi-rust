0c87b80286cb02640230abf2f6efde30a68ba75b
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2022-06-05 07:59:13 +0300

    More types

--- src/ckan.rs
+++ src/ckan.rs
@@ -1,10 +1,14 @@
-use reqwest::blocking::Client;
+use std::collections::HashMap;
+
+use reqwest::blocking::{multipart::Form, Client};
+
 use serde::Deserialize;
 use serde_json::Value;
 
 #[derive(Debug)]
 pub struct CKAN {
     url: String,
+    token: Option<String>,
     client: Client,
 }
 
@@ -13,15 +17,48 @@ impl CKAN {
         CKAN {
             url: url.trim_matches('/').to_owned(),
             client: Client::new(),
+            token: None
+        }
+    }
+
+    pub fn with_token(self, token: &str) -> Self {
+        CKAN {
+            token: Some(token.into()),
+            ..self
         }
     }
     pub fn url(&self) -> &str {
         &self.url
     }
 
-    pub fn invoke<T: for<'de> Deserialize<'de>>(&self, action: &Action) -> Response<T> {
+    pub fn invoke<T: for<'de> Deserialize<'de>>(&self, action: Action) -> Response<T> {
         let url = format!("{}/api/action/{}", self.url, &action.name);
-        match self.client.post(url).send() {
+        let mut req = self.client.post(url);
+        if let Some(token) = &self.token {
+            req = req.header(reqwest::header::AUTHORIZATION, token);
+        }
+
+        req = match action.params {
+            Params::Empty => req,
+            Params::Multipart(plain, files) => {
+                let mut form = Form::new();
+                for (k, v) in plain.into_iter() {
+                    form = form.text(k, v);
+                }
+
+                for (k, v) in files.into_iter() {
+                    form = match form.file(k, v) {
+                        Ok(form) => form,
+                        Err(err) => return Response::FileError(err.to_string()),
+                    };
+                }
+
+                req.multipart(form)
+            }
+            Params::Json(ref data) => req.json(data),
+        };
+
+        match req.send() {
             Ok(resp) => match resp.json::<Response<T>>() {
                 Ok(result) => result,
                 Err(err) => Response::DecodeError(err.to_string()),
@@ -39,6 +76,19 @@ pub enum Response<T> {
     StringError(String),
     ReqwestError(String),
     DecodeError(String),
+    FileError(String),
+}
+
+impl<T: std::fmt::Debug> Response<T> {
+    pub fn extract(self) -> Option<T> {
+        match self {
+            Response::Result(Success { result, .. }) => Some(result),
+            err => {
+                log::error!("Error during API request: {:?}", err);
+                None
+            }
+        }
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -56,22 +106,45 @@ pub struct Fail {
 #[derive(Debug)]
 pub struct Action {
     pub name: String,
+    pub params: Params,
+}
+
+impl Action {
+    pub fn new(name: &str, params: Params) -> Self {
+        Action {
+            name: name.into(),
+            params,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Params {
+    Empty,
+    Multipart(HashMap<String, String>, HashMap<String, String>),
+    Json(Value),
 }
 
 #[cfg(test)]
+#[allow(unused_variables)]
 mod tests {
-    use super::*;
+    // use super::*;
 
-    #[test]
-    fn test_name() {
-        let ckan = CKAN::new("http://localhost:5000");
+//     #[test]
+//     fn test_name() {
+//         let ckan = CKAN::new("http://localhost:5000");
 
-        let action = Action {
-            name: "status_show".into(),
-        };
+//         let mut plain: HashMap<String, String> = HashMap::new();
+//         plain.insert("rows".into(), "0".into());
+//         plain.insert("fl".into(), "id".into());
 
-        let result: Response<Value> = ckan.invoke(&action);
+//         let action = Action::new(
+//             "package_search",
+//             // Params::Json(serde_json::json!({"rows": 1, "fl": "id"})),
+//             Params::Multipart(plain, HashMap::new()),
+//         );
 
-        dbg!(result);
-    }
+//         let result: Response<Value> = ckan.invoke(action);
+//         // dbg!(result);
+//     }
 }
